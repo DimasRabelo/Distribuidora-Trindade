f8adf50b188c0d1bc4cbaf03e6bbf71731a61c14
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-02-09 18:23:35 -0300

    Alterando o formato do código de Html para php para otiimzar o código

---
+++ conteudo/banner.php
@@ -0,0 +1,5 @@
+<section class="banner">
+      <img src="img/Banner/banner1.png" alt="Banner promocional-1" />
+      <img src="img/Banner/banner2.png" alt="Banner promocional-2" />
+      <img src="img/Banner/banner3.png" alt="Banner promocional-3" />
+    </section>
---
+++ conteudo/topoMenu.php
@@ -0,0 +1,17 @@
+<header class="menu-inicio">
+    <div class="logogeral">
+      <img
+        src="img/logo-trindade.jpg"
+        class="logo"
+        alt="Logo Trindade Distribuidora de Ovos"
+      />
+    </div>
+    <nav>
+      <ul>
+        <li><a href="index.html" title="Página Inicial">Início</a></li>
+        <li><a href="produtos.html" title="Nossos Produtos">Produtos</a></li>
+        <li><a href="quemSomos.html" title="Sobre Nós">Quem Somos</a></li>
+        <li><a href="contato.html" title="Fale Conosco">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
