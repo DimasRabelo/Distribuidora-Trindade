3c1aa3311baa43724ed08c189afffea94dd30372
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-05-05 08:59:12 -0300

    Criando o Dashboard do site

---
+++ admin/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="br">
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Distribuidora Trindade</title>
+
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/estilo.css">
+</head>
+
+<body>
+    <header class="topo">
+        <img src="img/logo-trindade.jpg" alt="Imagem da Logo" class="logo">
+
+        <nav class="menu-superior">
+            <a href="index.php?p=dashboard">Dashboard</a>
+            <a href="index.php?p=funcionarios">Funcionários</a>
+            <a href="index.php?p=produtos">Produtos</a>
+            <a href="index.php?p=estoque">Estoque</a>
+            <a href="index.php?p=vendas">Vendas</a>
+            <a href="index.php?p=itensvendidos">Itens Vendidos</a>
+            <a href="index.php?p=contatoo">E-mails</a>
+            <a href="index.php?p=relatorio">Relatórios</a>
+            <a href="index.php?p=ajuda">Ajuda e Suporte</a>
+        </nav>
+    </header>
+   
+</body>
+<footer class="rodapé2">
+        <section>
+            <p>
+                DESENVOLVIDO POR DIMAS APARECIDO RABELO DE SOUZA WHATSAPP: 11972495017
+            </p>
+        </section>
+    </footer>
+
+
+</html>
