e6fec5895d2186001d4d7546ebc807a2e1c8dbbb
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-05-12 15:22:29 -0300

    Implementando o Dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,4 @@
 <!DOCTYPE html>
-<html lang="br">
-
-<!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
@@ -24,20 +21,73 @@
             <a href="index.php?p=estoque">Estoque</a>
             <a href="index.php?p=vendas">Vendas</a>
             <a href="index.php?p=itensvendidos">Itens Vendidos</a>
-            <a href="index.php?p=contatoo">E-mails</a>
+            <a href="index.php?p=contato">E-mails</a>
             <a href="index.php?p=relatorio">Relatórios</a>
             <a href="index.php?p=ajuda">Ajuda e Suporte</a>
         </nav>
     </header>
-   
+
+    <div class="box">
+        <!-- CONTEÚDO DAS PÁGINAS -->
+
+        <?php
+
+        switch ($pagina) {
+            case 'dashboard':
+
+                break;
+            case 'funcionarios':
+              
+                break;
+
+            case 'produtos':
+                
+                break;
+
+            case 'estoque':
+
+                break;
+            case 'vendas':
+                
+                break;
+            case 'itensvendidos':
+               
+                break;
+            case 'contato':
+               
+                break;
+
+            case 'relatorio':
+
+                break;
+            case 'ajuda':
+        }
+        ?>
+
+    </div>
+    </main>
+
+    <?php
+    if ($pagina == '') {
+        echo '<h2>Dashboard</h2>';
+    } else {
+        echo ('<h2>Página: ' . $pagina . '</h2>');
+    }
+
+    ?>
+
+
 </body>
-<footer class="rodapé2">
-        <section>
-            <p>
-                DESENVOLVIDO POR DIMAS APARECIDO RABELO DE SOUZA WHATSAPP: 11972495017
-            </p>
-        </section>
-    </footer>
+
+
+
+<!-- <footer class="rodapé2">
+    <section>
+        <p>
+            DESENVOLVIDO POR DIMAS APARECIDO RABELO DE SOUZA WHATSAPP: 11972495017
+        </p>
+    </section>
+</footer> -->
 
 
 </html>
