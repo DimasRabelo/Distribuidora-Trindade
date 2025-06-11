91c20b98094fb85b8d80a020d31886c716d2f00b
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-06-11 09:56:18 -0300

    update

--- admin/index.php
+++ admin/index.php
@@ -31,6 +31,7 @@ $nomePaginaAtual = $nomesPaginas[$pagina] ?? strtoupper($pagina);
     <title>Distribuidora Trindade</title>
 
     <link rel="stylesheet" href="./css/reset.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/estilo.css">
 </head>
 
@@ -97,6 +98,11 @@ $nomePaginaAtual = $nomesPaginas[$pagina] ?? strtoupper($pagina);
         <?php echo strtoupper($nomePaginaAtual); ?>
     </span>
 </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 
 </body>
 
